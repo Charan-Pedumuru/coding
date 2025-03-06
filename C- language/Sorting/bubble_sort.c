cd65feeacb023d38beea04c9f40a06f0f59e70c0
Charan Pedumuru <charan17171@gmail.com>
2025-03-06 15:34:31 +0530

    Modify bubble_sort

--- C- language/Sorting/bubble_sort.c
+++ C- language/Sorting/bubble_sort.c
@@ -4,16 +4,15 @@ int main()
 {
 	int arr[]={56,6,17,9};
 	int n= sizeof(arr)/sizeof(arr[0]);
+    int temp=0;
 
 	for (int i=0;i<n;i++) {
-		scanf("%d", &arr[i]);
-	}
-
-	for (int i=0;i<n-1;i++) {
-		for (int j=i;j<n;j++) {
-			if(arr[i]>arr[j])
+		for (int j=i;j<n-1;j++) {
+			if(arr[j]>arr[j+1])
 			{
-				arr[i]=arr[i]+arr[j]-(arr[j]=arr[i]);
+				temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
 			}
 		}
 	}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
 
-void replace(char *c)
-{
-    while(*c!='\0')
-    {
-        if(*c=='e'|| *c=='o')
-        {
-            *c='A';
-            
-        }
-        c++;
-    }
-}
-
 int main()
 {
-    char str[]="Hello World";
-    replace(str);
-    printf("%s",str);
-    return 0;
+    int arr[]={3,4,7,6};
+    int size= sizeof(arr)/sizeof(arr[0]);
+    int temp=0;
+
+    for(int i=0;i<size;i++)
+    {
+        for(int j=i;j<size-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
 }
